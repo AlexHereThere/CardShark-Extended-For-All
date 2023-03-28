9d6e82e8a709ceab94ff3447dff53c5f0426a148
Alejandro Castro Renteria <a1183297@uabc.edu.mx>
2023-03-27 18:15:38 -0700

    primera version

---
+++ Baraja.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Baraja {
+  private ArrayList<Carta> mazo;
+
+  public Baraja() {
+    this.mazo = new ArrayList<>();
+    inicializa();
+  }
+
+  private void inicializa() {
+    for (Figura f: Figura.values()) {
+      for( int v= 2 ; v <= 14 ; v++) {
+        try {
+          mazo.add(new Carta(v,f));
+        } catch (FueraDeRangoException e) {
+          System.out.println(e);
+        }
+      }
+    }
+  }
+  /**
+   * Nuevo metodo: hace mas grande el mazo agregando otra baraja a la baraja ya existente
+   */
+  public void agregarOtraBaraja()
+  {
+   inicializa();   
+  }
+
+  public void barajar() {
+    Collections.shuffle(mazo);
+  }
+
+  public ArrayList<Carta> repartir(int n) {
+    ArrayList<Carta> mano = new ArrayList<>();
+    for(int i=1; i<=n; i++) {
+      mano.add(mazo.remove(0));
+    }
+    return mano;
+  }
+
+  @Override
+  public String toString() {
+    StringBuffer mazoStr = new StringBuffer();
+    for (Carta c : mazo) {
+      mazoStr.append(c.toString()+"\n");
+    }
+    return mazoStr.toString();
+  }
+
+
+}
